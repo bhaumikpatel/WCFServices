6e0435817efa1b421435913b501412704bef6db9
Bhaumik Patel <bhaumik1008@yahoo.com>
2012-08-24 14:41:42 +0530

    Add WCF Services

---
+++ WCFService/ICurrencyConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ServiceModel;
+using System.Web.Mvc;
+using System.ServiceModel.Activation;
+
+namespace WCFService
+{
+    [ServiceContract]
+    public interface ICurrencyConverter
+    {
+        [OperationContract]
+        IList<SelectListItem> GetCountryDropDown();
+
+        [OperationContract]
+        decimal ConvertToGoogle(decimal amount, string fromCurrency, string toCurrency);
+
+        [OperationContract]
+        decimal ConvertToYahoo(decimal amount, string fromCurrency, string toCurrency);
+    }
+}
